24b5084a6c6371321a3c6a2b7ff9992af00df1be
Joel Bohman <joelboh@gmail.com>
2011-07-06 23:50:04 +0200

    use interfaces and _declarations from zope.registry

--- src/zope/component/_declaration.py
+++ src/zope/component/_declaration.py
@@ -13,48 +13,8 @@
 ##############################################################################
 """Adapter declarations
 """
-import types
-import sys
-
-class adapter:
-
-    def __init__(self, *interfaces):
-        self.interfaces = interfaces
-
-    def __call__(self, ob):
-        if isinstance(ob, _class_types):
-            ob.__component_adapts__ = _adapts_descr(self.interfaces)
-        else:
-            ob.__component_adapts__ = self.interfaces
-
-        return ob
-
-def adapts(*interfaces):
-    frame = sys._getframe(1)
-    locals = frame.f_locals
-
-    # Try to make sure we were called from a class def. In 2.2.0 we can't
-    # check for __module__ since it doesn't seem to be added to the locals
-    # until later on.
-    if (locals is frame.f_globals) or (
-        ('__module__' not in locals) and sys.version_info[:3] > (2, 2, 0)):
-        raise TypeError("adapts can be used only from a class definition.")
-
-    if '__component_adapts__' in locals:
-        raise TypeError("adapts can be used only once in a class definition.")
-
-    locals['__component_adapts__'] = _adapts_descr(interfaces)
-
-def adaptedBy(ob):
-    return getattr(ob, '__component_adapts__', None)
-
-_class_types = type, types.ClassType
-
-class _adapts_descr(object):
-    def __init__(self, interfaces):
-        self.interfaces = interfaces
-
-    def __get__(self, inst, cls):
-        if inst is None:
-            return self.interfaces
-        raise AttributeError('__component_adapts__')
+from zope.registry._declaration import adapter
+from zope.registry._declaration import adapts
+from zope.registry._declaration import adaptedBy
+from zope.registry._declaration import _adapts_descr
+from zope.registry._declaration import _class_types
